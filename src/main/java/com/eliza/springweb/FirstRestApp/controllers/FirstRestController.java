61a92598da732e9d479337263fed9a5df02d2f55
Lisa <elbondarenko04@gmail.com>
2024-10-09 22:42:03 +0300

    return string message

--- src/main/java/com/eliza/springweb/FirstRestApp/controllers/FirstRestController.java
+++ src/main/java/com/eliza/springweb/FirstRestApp/controllers/FirstRestController.java
@@ -1,16 +1,19 @@
 package com.eliza.springweb.FirstRestApp.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController //@Controller + @ResponseBody над каждым методом
 @RequestMapping("/api")
 public class FirstRestController {
 
-    @ResponseBody
+//    @ResponseBody
+    @GetMapping("/sayHello")
     public String sayHello() {
-        return "";
-
+        return "Hello world";
     }
+
+
 }
